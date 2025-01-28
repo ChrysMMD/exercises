1ad7037d6cf6ac6a4ae8912b36c9d8dab02bec7b
Chrys <chjo0001@stud.kea.dk>
2025-01-28 17:28:48 +0100

    create exercise lib

---
+++ tilfærdig_tal/script.js
@@ -0,0 +1,13 @@
+// console.log(Math.floor(Math.random() * 100));
+
+// function randomNumber() {
+//   console.log(Math.floor(Math.random() * 100));
+// }
+
+//Variable med en value
+let randomNumber = Math.floor(Math.random() * 100)
+
+//output til HTML
+document.getElementById("output").innerHTML = randomNumber
+
+console.log(randomNumber)
